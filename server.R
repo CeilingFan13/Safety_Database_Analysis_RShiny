57f22a6b0ad63582915a615b5dfcf52246e6491b
Shuang Fan <shuang.fan.e@gmail.com>
2022-05-10 12:30:57 -0400

    Layout update
    5/10/22 12pm: updated the layering, and enable filter by specific categories.

--- server.R
+++ server.R
@@ -1,26 +1,50 @@
 library(readxl)
 library(shiny)
 library(ggplot2)
-
+library(Dict)
+library(stringr)
+ctcae <- 
 server <- function(input, output, session) {
   # enable column and row selection
   info <- eventReactive(input$choice, {
     inFile <- input$file1
     req(inFile)
-    df <- read.table(input$file1$datapath,
+    df <- read.csv(input$file1$datapath,
                      header = input$header,
-                     sep = input$sep)
-    c_vars <- names(df)
+                     sep = input$sep, blank.lines.skip = TRUE)
+    #names(df) <- str_replace_all(names(df), c(" " = "_"))
+    # change all PT and SOC to lowercase
+    df$PT <- tolower(df$PT)
+    df$SOC <- tolower(df$SOC)
+    # get list of unique items to filter
+    subject_list <- c(unique(df$Subject.ID))
+    site_list <- c(unique(df$Site))
+    pt_list <- c(unique(df$PT))
+    soc_list <- c(unique(df$SOC))
+    #c_vars <- names(df)
     r_vars <- row.names(df)
-    updateSelectInput(session, "columns", 
-                      "Columns to show", choices = c_vars)
+    #updateSelectInput(session, "columns", 
+                      #"Columns to show", choices = c_vars)
     updateSelectInput(session, "rows", 
-                      "Rows to show", choices = r_vars)
+                      "Rows to show", choices = r_vars,
+                      selected = r_vars)
+    updatePickerInput(session, "subject", 
+                      "Subject", choices = subject_list,
+                      selected = subject_list)
+    updatePickerInput(session, "site", 
+                      "Site", choices = site_list,
+                      selected = site_list)
+    updatePickerInput(session, "preferred_term", 
+                      "Preferred Term", choices = pt_list,
+                      selected = pt_list)
+    updatePickerInput(session, "organ_class", 
+                      "System Organ Class", choices = soc_list,
+                      selected = soc_list)
     return(df)
   })
    output$contents <- DT::renderDataTable({
      data <- info()
-     data <- subset(data[input$rows,], select = input$columns)
+     data <- subset(data[input$rows,])#, select = input$columns)
      return(data)
   })
 }
--- ui.R
+++ ui.R
@@ -1,52 +1,161 @@
 library(shiny)
 library(ggplot2)
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Safety Database Analysis"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      
-      # select a file to be analyzed
-      fileInput(inputId = "file1",
-                label = "Choose a CSV file",
-                multiple = FALSE,
-                accept = c("text/csv", 
-                          ".csv")),
-    
-      # horizontal line
-    #tag$hr(),
-    
-#    fluidRow(
-#      column(4, selectInput(c("All", unique(as.character(df$AE))))
-#    ),
-    
-    # check if file has header
-    checkboxInput(
-      inputId = "header",
-      label = "Check if the file contains header",
-      TRUE),
-    
-    radioButtons(inputId = "sep", label = "Type of Separator",
-      choices = c(Comma = ",", Semicolon = ";", Tab = "\t", None = ""),
-      selected = ","),
-    actionButton("choice", "incorporate external information"),
+library(shinyWidgets)
 
-    selectInput(inputId = "columns", 
-                       label = "Columns to show",
-                       choices = NULL, multiple = TRUE),
+ui <- navbarPage(title = "Safety Database",
+                 fluidRow(
+                   column(3, 
+                          pickerInput(
+                     inputId = "subject",
+                     label = "Subject",
+                     choices = NULL,
+                     multiple = TRUE,
+                     options = list("actions-box" = TRUE)
+                   )),
+                   column(3, 
+                          pickerInput(
+                            inputId = "site",
+                            label = "Site",
+                            choices = NULL,
+                            multiple = TRUE,
+                            options = list("actions-box" = TRUE)
+                          )),
+                   column(3, 
+                          pickerInput(
+                            inputId = "preferred_term",
+                            label = "Preferred Term",
+                            choices = NULL,
+                            multiple = TRUE, 
+                            options = list("actions-box" = TRUE)
+                          )),
+                   column(3, 
+                          pickerInput(
+                            inputId = "organ_class",
+                            label = "System Organ Class",
+                            choices = NULL,
+                            multiple = TRUE,
+                            options = list("actions-box" = TRUE)
+                          ))
+                 ),
+                 # create summary page for CSV upload and display
+                 tabPanel(
+                   title = "Summary",
+                   # Sidebar with a slider input for number of bins
+                   sidebarLayout(
+                     sidebarPanel(
+                       width = 2,
+                       # select a file to be analyzed
+                       fileInput(
+                         inputId = "file1",
+                         label = "Choose a CSV file",
+                         multiple = FALSE,
+                         accept = c("text/csv", ".csv")
+                       ),
+                       # check if file has header
+                       checkboxInput(inputId = "header",
+                                     label = "Check if the file contains header",
+                                     TRUE),
+                       radioButtons(
+                         inputId = "sep",
+                         label = "Type of Separator",
+                         choices = c(
+                           Comma = ",",
+                           Semicolon = ";",
+                           Tab = "\t",
+                           None = ""
+                         ),
+                         selected = ","
+                       ),
+                       actionButton("choice", "Update"),
+                       
+                       #selectInput(inputId = "columns",
+                       #label = "Columns to show",
+                       #choices = NULL, multiple = TRUE),
+                       
+                       selectInput(
+                         inputId = "rows",
+                         label = "Rows to show",
+                         choices = NULL,
+                         multiple = TRUE
+                       )
+                
+                       
+                     ),
+                     mainPanel(
+                       width = 10,
+                       tabsetPanel(
+                       id = "tabs",
+                       tabPanel("original_data",
+                                DT::dataTableOutput("contents")),
+                       tabPanel("page2"),
+                       tabPanel("page3")
+                     ))
+                   )
+                   
+                 ),
+                 
+                 # Second page
+                 tabPanel(title = "Page2")
+                 
+                 )
 
-    selectInput(inputId = "rows",
-                label = "Rows to show",
-                choices = NULL, multiple = TRUE)
-    
-  ),
-  # Show a plot of the generated distribution
-  mainPanel(DT::dataTableOutput("contents"))
-  
-))
+# ui <- fluidPage(
+#   # Application title
+#   titlePanel("Safety Database Analysis"),
+#   
+#   # Sidebar with a slider input for number of bins
+#   sidebarLayout(
+#     sidebarPanel(
+#       # select a file to be analyzed
+#       fileInput(
+#         inputId = "file1",
+#         label = "Choose a CSV file",
+#         multiple = FALSE,
+#         accept = c("text/csv",
+#                    ".csv")
+#       ),
+#       
+#       
+#       # check if file has header
+#       checkboxInput(inputId = "header",
+#                     label = "Check if the file contains header",
+#                     TRUE),
+#       
+#       radioButtons(
+#         inputId = "sep",
+#         label = "Type of Separator",
+#         choices = c(
+#           Comma = ",",
+#           Semicolon = ";",
+#           Tab = "\t",
+#           None = ""
+#         ),
+#         selected = ","
+#       ),
+#       actionButton("choice", "incorporate external information"),
+#       
+#       #selectInput(inputId = "columns",
+#       #label = "Columns to show",
+#       #choices = NULL, multiple = TRUE),
+#       
+#       selectInput(
+#         inputId = "rows",
+#         label = "Rows to show",
+#         choices = NULL,
+#         multiple = TRUE
+#       ),
+#       tabsetPanel(
+#         id = "tabs",
+#         tabPanel("original_data",
+#                  DT::dataTableOutput("contents")),
+#         tabPanel("page2"),
+#         tabPanel("page3")
+#       )
+#       
+#     ),
+#     # Show a plot of the generated distribution
+#     mainPanel()
+#     
+#   ))
 
 
